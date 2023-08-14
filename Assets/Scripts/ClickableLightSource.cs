3dfbf7e01037614a4a2473d9ad88d5e38405c553
Maciej Owczarzak <omaciejj@gmail.com>
2023-08-14 23:46:30 +0200

    feat: actually made a working ClickableLightSource instead of whatever I made earlier

--- Assets/Scripts/ClickableLightSource.cs
+++ Assets/Scripts/ClickableLightSource.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class ClickableLightSource : MonoBehaviour, IPointerClickHandler
+public class ClickableLightSource : MonoBehaviour
 {
-    public void OnPointerClick(PointerEventData eventData)
+    private void Awake()
     {
-        lightSourceEnabled = !lightSourceEnabled;
-
-        Collider2D lightCollider = transform.parent.GetComponent<Collider2D>();
-
-        lightCollider.enabled = lightSourceEnabled;
+        parentCollider = transform.parent.GetComponent<Collider2D>();
+        parentCollider.enabled = isActive;
     }
 
-    [SerializeField] private bool lightSourceEnabled;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit2D[] allHits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            foreach(RaycastHit2D hit in allHits)
+            {
+                if(hit.collider == null) continue;
+
+                if (hit.collider.gameObject == gameObject)
+                {
+                    isActive = !isActive;
+
+                    parentCollider.enabled = isActive;
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private Collider2D parentCollider;
+
+    [SerializeField] private bool isActive;
 }
