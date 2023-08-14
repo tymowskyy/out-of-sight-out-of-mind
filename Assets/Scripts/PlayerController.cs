b59f4900dfe10ac61f819b7cd5b12fed5d72fc8f
Maciej Owczarzak <omaciejj@gmail.com>
2023-08-14 22:01:17 +0200

    feat: the player now squeezes/squashes when jumping and falling/landing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -8,6 +7,8 @@ public class Movement : MonoBehaviour
     private void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+
+        initializeDefaults();
     }
 
     private void Update()
@@ -52,11 +53,42 @@ public class Movement : MonoBehaviour
                 rb.gravityScale = 1f;
             }
         }
+
+        if(rb.velocity.y > 1.5f)
+        {
+            transform.localScale = new Vector3(jumpSqueezeScale, defaultScale.y, defaultScale.z);
+        } 
+        
+        else if(Mathf.Abs(rb.velocity.y) < 0.2f && lastFrameVelocity.y < -1.5f)
+        {
+            landSquashTimer = landSquashAnimationTime;
+            transform.localScale = new Vector3(defaultScale.x, landSquashScale, defaultScale.z);
+            transform.position = new Vector3(transform.position.x, transform.position.y - defaultScale.y + landSquashScale, transform.position.z);
+        }
+
+        else if(landSquashTimer <= 0f)
+        {
+            if(transform.localScale.y == landSquashScale)
+            {
+                transform.position = new Vector3(lastFramePosition.x, lastFramePosition.y + 0.1f, lastFramePosition.z);
+            }
+
+            transform.localScale = defaultScale;
+        }
+
+        lastFrameVelocity = rb.velocity;
+        lastFramePosition = transform.position;
     }
 
     private void FixedUpdate()
     {
         horizontalMovement();
+    }
+
+    private void initializeDefaults()
+    {
+        defaultGravityScale = rb.gravityScale;
+        defaultScale = transform.localScale;
     }
 
     private void horizontalMovement()
@@ -86,9 +118,11 @@ public class Movement : MonoBehaviour
     {
         jumpTimer -= Time.deltaTime;
         jumpInputTimer -= Time.deltaTime;
+        landSquashTimer -= Time.deltaTime;
 
         jumpTimer = Mathf.Max(jumpTimer, 0f);
         jumpInputTimer = Mathf.Max(jumpInputTimer, 0f);
+        landSquashTimer = Mathf.Max(landSquashTimer, 0f);
     }
 
     private bool canJump()
@@ -109,6 +143,15 @@ public class Movement : MonoBehaviour
     private float jumpTimer;
     private float jumpInputTimer;
 
+    private float landSquashTimer;
+
+    private Vector3 defaultScale;
+
+    private float defaultGravityScale;
+
+    private Vector2 lastFrameVelocity;
+    private Vector3 lastFramePosition;
+
     [Header("Run")]
     [SerializeField] private float maxRunSpeed;
     [SerializeField] private float accelerationRate;
@@ -119,8 +162,15 @@ public class Movement : MonoBehaviour
     [SerializeField] private float coyoteTime;
     [SerializeField] private float maxJumpInputDelay; //if you press the jump input maxJumpInputDelay seconds or less before hitting the ground,
                                                       //you will automatically jump on the next frame
+
+    [SerializeField] private float jumpSqueezeScale; //the player's X scale is changed to this when he jumps
+    [SerializeField] private float landSquashScale; //the player's Y scale is changed to this when he lands
+    [SerializeField] private float landSquashAnimationTime; //for how long should the okayer be squashed after landing
+
+    [Header("Gravity")]
     [SerializeField] private float peakGravityScale;
     [SerializeField] private float fallingGravityScale;
+
 
     [Header("Checks")]
     [SerializeField] private Transform groundCheckPos;
