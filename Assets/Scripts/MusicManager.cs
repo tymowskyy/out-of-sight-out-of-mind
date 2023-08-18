010345948abfb799f1cba16da2c8b934297831f0
Maciej Owczarzak <omaciejj@gmail.com>
2023-08-18 15:56:42 +0200

    fix: fix LevelManager and Music spawning again after restarting level 1 fr this time

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class MusicManager : MonoBehaviour
 {
-    private void Awake()
+    private void Start()
     {
+        if(GameObject.FindGameObjectsWithTag("Music").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
         DontDestroyOnLoad(gameObject);
     }
 }
