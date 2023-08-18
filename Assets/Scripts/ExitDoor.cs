ee490552e5b5406fdb4607be7224afd13cb48dff
tymowskyy <tymowskyy@gmail.com>
2023-08-19 01:58:22 +0200

    fix: exit doors finnaly work (I hope) ([please be true])

--- Assets/Scripts/ExitDoor.cs
+++ Assets/Scripts/ExitDoor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour
+public class ExitDoor : MonoBehaviour
 {
     private void Awake()
     {
