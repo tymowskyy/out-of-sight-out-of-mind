60141e966c4719718ed3fc00ae475201d9f155d0
tymowskyy <tymowskyy@gmail.com>
2023-08-19 03:48:15 +0200

    feat: settings, volume slider

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject startingScreen;
+    [SerializeField] private GameObject settings;
+    [SerializeField] private GameObject levelSellector;
+
     public void PlayGame()
     {
         LevelManager.instance.ContinueGame();
     }
+
+    public void OpenSettings()
+    {
+        startingScreen.SetActive(false);
+        settings.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        startingScreen.SetActive(true);
+        settings.SetActive(false);
+    }
+
+    public void UpdateVolume(float volume)
+    {
+        MusicManager.instance.UpdateVolume(volume);
+    }
+
 }
