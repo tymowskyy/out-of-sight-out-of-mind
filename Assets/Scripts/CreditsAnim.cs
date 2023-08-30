00454cb318f13b03a19cf96f5cb77c8798c8e470
Maciej Owczarzak <omaciejj@gmail.com>
2023-08-30 14:56:47 +0200

    feat: you can now skip the credits animation along with the wait after it by pressing space

--- Assets/Scripts/CreditsAnim.cs
+++ Assets/Scripts/CreditsAnim.cs
@@ -13,22 +13,41 @@ public class CreditsAnim : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (!done)
+            {
+                //Skip the animation
+                done = true;
+
+                tmpro.text = creditsText;
+            }
+
+            else
+            {
+                //Skip the wait after the animation ends
+                afterAnimTimeout = 0f;
+            }
+        }
+
         if (!done)
         {
             charTimer += Time.deltaTime;
 
             if (charTimer >= charDelay)
             {
-                tmpro.text += text[iter];
+                tmpro.text += creditsText[nextCharToDisplay];
                 charTimer = 0f;
-                iter++;
+                nextCharToDisplay++;
             }
 
-            if (iter >= text.Length)
+            if (nextCharToDisplay >= creditsText.Length)
             {
                 done = true;
             }
-        } else
+        } 
+        
+        else
         {
             afterAnimTimeout -= Time.deltaTime;
 
@@ -41,13 +60,13 @@ public class CreditsAnim : MonoBehaviour
 
     private bool done = false;
 
+    private int nextCharToDisplay = 0;
     private float charTimer;
-    private float afterAnimTimeout = 7.34f;
 
     [SerializeField] private float charDelay;
+    [SerializeField] private float afterAnimTimeout;
 
     private TextMeshProUGUI tmpro;
 
-    private int iter = 0;
-    private const string text = "Thanks for playing!\n\nMusic and audio:\r\nM4K1\r\n\r\nCoding:\r\nGonk\r\nTymowskyy\r\nEnard\r\n\r\nArt:\r\nOrles\r\nM4K1";
+    private const string creditsText = "Thanks for playing!\n\nMusic and audio:\r\nM4K1\r\n\r\nCoding:\r\nGonk\r\nTymowskyy\r\nEnard\r\n\r\nArt:\r\nOrles\r\nM4K1";
 }
